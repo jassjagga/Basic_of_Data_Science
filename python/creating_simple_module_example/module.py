00cc681a914a76bcf0b3c76e35f01af64698a907
Jaswinder Singh <51167676+jassjagga@users.noreply.github.com>
2022-05-09 07:06:22 -0400

    Add files via upload

---
+++ python/creating_simple_module_example/module.py
@@ -0,0 +1,14 @@
+import simplefunction
+#other way to import fucntion is also like
+#import simplefunction as fun
+
+#if the function is in another directory then you have to provide th loaction then
+#import (FOLDER NAME).simplefunction as fun
+
+#if the code is totally in deferent folder or location then we have to use system path
+#eg import sys
+#sys.path.append("C:\pycode")
+#import simplefunction as fun
+#
+area = simplefunction.cal_squ_area(5)
+print(area)
---
+++ python/creating_simple_module_example/simplefunction.py
@@ -0,0 +1,4 @@
+def cal_tri_area(base,height):
+    return 1/2*(base*height)
+def cal_squ_area(length):
+    return length*length
