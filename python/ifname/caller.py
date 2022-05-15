17373a2764a2fd5e3f46f9a37320c323f34b8242
Jaswinder Singh <51167676+jassjagga@users.noreply.github.com>
2022-05-14 20:20:02 -0400

    Add files via upload

---
+++ python/ifname/caller.py
@@ -0,0 +1,4 @@
+import ifname_main
+print("I am in caller.py")
+Ca_area = ifname_main.cal_tri_area(5,10)
+print("caller area :", Ca_area)
---
+++ python/ifname/ifname_main.py
@@ -0,0 +1,8 @@
+def cal_tri_area(base,height):
+    print("__name__:",__name__)
+    return 1/2*(base*height)
+
+if __name__ =="__main__":
+    print("I am in ifname_main")
+    a=cal_tri_area(10,20)
+    print("area : ",a)
